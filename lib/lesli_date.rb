99bf4198263757a8b9e19ba3219abf62a52ee87e
Luis Donis <ldonis.emc2@gmail.com>
2026-01-16 22:25:18 -0600

    test compatibility functions

--- lib/lesli_date.rb
+++ lib/lesli_date.rb
@@ -6,4 +6,10 @@ require "lesli_date/version"
 #require "lesli_date/engine"
 
 module LesliDate
+    class Compatibility
+        def self.db_now
+            return 'CURRENT_TIMESTAMP' if ActiveRecord::Base.connection.adapter_name == "SQLite"
+            return 'NOW()' #postgresql
+        end
+    end 
 end
